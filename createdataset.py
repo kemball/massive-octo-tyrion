d8f9770119a1b770df2a87113011ad94727104bf
Jeremy Kemball <jeremykemball@wirelessprvnat-172-16-213-175.near.illinois.edu>
2013-04-25 13:58:01 -0500

    Added some functions, they seem to work

--- createdataset.py
+++ createdataset.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python
+import random
 
+def create_motif(length=10):
+#should return a PWM
+    pass
 
+def create_benchmark(icpc=2,ml=8,sl=500,sc=10):
+    charset = "ACGT"
+    sequences = [generaterandom(length=sl) for x in range(0,sc)]
+    pass
 
+def write_fasta(filename,strings,identifier="sequence"):
+    f = open(filename,'w')
+    for (num,bases) in enumerate(strings):
+        f.write('>'+str(identifier) + str(num)+'\n')
+        while len(bases)>=80:
+            f.write(bases[0:80])
+            f.write('\n')
+            bases = bases[80:]
+        f.write(bases)
+    f.close()
 
+def generaterandom(length=10,charset="ACGT"):
+    return "".join([ random.choice(charset) for x in range(0,length)])
 
 
 
