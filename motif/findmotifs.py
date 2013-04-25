0cdf5552f75969310110fac527cfdbabc549a62a
Jeremy Kemball <jeremykemball@JHARTIGAN.local>
2013-04-25 13:00:54 -0500

    Added a some more placeholders. Excitin!

---
+++ motif/findmotifs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+
+
+
+
+
+if __name__=="__main__":
+    print "I should be called with some files, then find some motifs!"
---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+
+
+
+
+
+if __name__=="__main__":
+    print "make some tests I guess?"
