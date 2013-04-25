fdab7e50f507c6801a6b32760ca912b73d316a8b
Jeremy Kemball <jeremykemball@wirelessprvnat-172-16-213-175.near.illinois.edu>
2013-04-25 14:32:35 -0500

    gitignore, some information math

--- createdataset.py
+++ createdataset.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 import random
+from math import log
+charset = "ACGT"
+def information(column):
+    #notable: assumes bases are uniform
+    return sum(map(lambda x:x and x*log(len(charset)*x),column))
 
-def create_motif(length=10):
-#should return a PWM
+def create_motif(icpc, length, charset=charset):
+#should return a PWM with
     pass
 
 def create_benchmark(icpc=2,ml=8,sl=500,sc=10):
-    charset = "ACGT"
+    charset = charset
     sequences = [generaterandom(length=sl) for x in range(0,sc)]
     pass
 
@@ -21,7 +26,7 @@ def write_fasta(filename,strings,identifier="sequence"):
         f.write(bases)
     f.close()
 
-def generaterandom(length=10,charset="ACGT"):
+def generaterandom(length=10,charset=charset):
     return "".join([ random.choice(charset) for x in range(0,length)])
 
 
