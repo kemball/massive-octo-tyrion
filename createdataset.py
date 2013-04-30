61449cb3bd1fa54f5cc76d72762e9f15f31ed774
Jeremy Kemball <jeremykemball@JHARTIGAN.local>
2013-04-29 23:54:30 -0500

    more changes and stuff

--- createdataset.py
+++ createdataset.py
@@ -14,9 +14,9 @@ def sample_motif(motif,charset=charset):
     return "".join([charset[x] for x in map(sample_column,motif)])
 
 def sample_column(column,charset=charset):
-    total = random.randint(0,sum(column))
+    total = random.randint(1,sum(column))
     for (letter,thing) in enumerate(column):
-        if total<thing:
+        if total<=thing:
             return letter
         else:
             total -= thing
@@ -40,16 +40,35 @@ def create_motif(icpc, length, charset=charset):
             random.shuffle(motif)
         motif = [ map(lambda x: int(x*10),col) for col in motif]
     else:
-        motif = [[1]+[0]*len(charset) for x in charset]
+        motif = [[1]+[0]*(len(charset)-1) for x in range(0,length)]
         map(random.shuffle,motif)
         random.shuffle(motif)
     return motif
 
 def create_benchmark(icpc=2,ml=8,sl=500,sc=10):
-    charset = charset
-    sequences = [generaterandom(length=sl) for x in range(0,sc)]
+    sequences = [generaterandom(sl) for x in range(0,sc)]
     motif = create_motif(icpc,ml)
-    pass
+    offsets = []
+    for x in range(0,sc):
+        offset = random.randint(0,sl-ml)
+        offsets.append(offset)
+        q = sample_motif(motif)
+        sequences[x] = sequences[x][:offset]+q+sequences[x][offset+ml:]
+        if icpc == 2:
+            assert(q in sequences[x])
+    import os
+    fileprefix = 'icpc'+str(icpc)+'ml'+str(ml)+'sl'+str(sl)+'sc'+str(sc)+'0'
+    while fileprefix in os.listdir('datasets'):
+        fileprefix[-1] = str(int(fileprefix[-1])+1)
+    os.mkdir('datasets/'+fileprefix)
+
+    sitefile = open('datasets/'+fileprefix+'/sites.txt',w)
+    for site in offsets:
+        sitefile.write(str(site))
+    finally:
+        sitefile.close()
+    #todo: write motifs, motiflength, sequences.fa
+
 
 def write_fasta(filename,strings,identifier="sequence"):
     f = open(filename,'w')
