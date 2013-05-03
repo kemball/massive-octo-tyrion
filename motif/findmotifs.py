a9ebbbdab1a72f39fe3bfe74b668a795b0eb8f19
Jeremy Kemball <jkemball@gmail.com>
2013-05-02 20:15:19 -0500

    gen_pat progress

--- motif/findmotifs.py
+++ motif/findmotifs.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 from motifs import read_fasta
 
+charset = 'ACGT'
+
 def cheapofind(length,sequences):
     lmersets = []
     for seq in sequences:
         lmers = [seq[x:length+x] for x in range(0,len(seq)-length+1)]
         lmersets.append(set(lmers))
-    return lmersets
     return reduce(lambda x,y: x&y, lmersets)
+
+def gen_pat(sequences,offsets,mlength):
+    szip = zip(sequences,offsets)
+    for x in range(0,mlength):
+        letters = map(lambda z: z[0][z[1]+x],szip)
+        col = [0]*len(charset)
+        for num,char in enumerate(charset):
+            col[num] = string.count(letters,char)
+
 
 
 
